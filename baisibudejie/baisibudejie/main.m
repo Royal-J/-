e553b226ecb434402d066d002791f12473ea7b0e
hao <123@126.com>
2016-11-21 14:12:19 +0800

    初始化项目

---
+++ baisibudejie/baisibudejie/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  baisibudejie
+//
+//  Created by HJY on 2016/11/21.
+//  Copyright © 2016年 HJY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
